500b495c0a6ef6d08571c805b9074b8491d79fdb
xdpsee <xdpsee@gmail.com>
2018-11-06 00:10:12 +0800

    load indicator

--- src/components/reader/Reader.vue
+++ src/components/reader/Reader.vue
@@ -10,6 +10,10 @@
       </div>
     </div>
     <menu-bar :isShowing="ifShowTitleAndMenu" ref="menuBar"></menu-bar>
+    <div class="load-indicator" v-show="!ifLoadFinished">
+      <img src="/static/ajax-loader.gif" alt="正在加载,请稍后">
+      <span v-show="ifError">加载失败,请刷新页面重试!</span>
+    </div>
   </div>
 </template>
 
@@ -26,20 +30,26 @@ export default {
   },
   data () {
     return {
-      ifShowTitleAndMenu: false
+      ifShowTitleAndMenu: false,
+      ifLoadFinished: false,
+      ifError: false
     }
   },
   methods: {
     showEpub () {
       let path = '/static/epubs/' + this.$route.params.id + '.epub'
-      console.log(path)
-      this.book = new Epub(path, {restore: true})
-      console.log(this.book)
-      this.redition = this.book.renderTo('page', {
-        width: window.innerWidth,
-        height: window.innerHeight
+      this.book = new Epub({retore: true})
+      this.book.open(path).then((data) => {
+        this.ifLoadFinished = true
+        this.redition = this.book.renderTo('page', {
+          width: window.innerWidth,
+          height: window.innerHeight
+        })
+        this.redition.display()
+      }, (reason) => {
+        console.log(reason)
+        this.ifError = true
       })
-      this.redition.display()
     },
     prevPage () {
       if (this.redition) {
@@ -86,6 +96,23 @@ export default {
       .right {
         flex: 0 0 px2rem(150);
       }
+    }
+  }
+  .load-indicator {
+    position: absolute;
+    left: 50%;
+    top: px2rem(260);
+    z-index: 200;
+    width: px2rem(200);
+    height: px2rem(200);
+    margin-left: px2rem(-100);
+    margin-top: px2rem(-100);
+    text-align: center;
+    span {
+      display: block;
+      color: #333333;
+      font-size: 0.4em;
+      margin-top: px2rem(20);
     }
   }
 }
